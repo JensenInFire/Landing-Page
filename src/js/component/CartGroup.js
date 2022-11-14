3d32b819d01a142e3e109631ba14dd17b2d62bcb
JensenInFire <alimendezgt@gmail.com>
2022-11-14 21:41:11 +0000

    Landing Page

---
+++ src/js/component/CartGroup.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { Card } from "./Card.js";
+
+export const CartGroup = () => {
+	return (
+		<div className="container p-0 d-flex mt-4 mb-3">
+			<div className="row">
+				<Card
+					imgUrl="https://fondosmil.com/fondo/3879.jpg"
+					tittle="Card Tittle"
+					description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+					urlButton="..."
+					labelButton="Find Out More!"
+				/>
+				<Card
+					imgUrl="https://fondosmil.com/fondo/3879.jpg"
+					tittle="Card Tittle"
+					description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+					urlButton="..."
+					labelButton="Find Out More!"
+				/>
+				<Card
+					imgUrl="https://fondosmil.com/fondo/3879.jpg"
+					tittle="Card Tittle"
+					description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+					urlButton="..."
+					labelButton="Find Out More!"
+				/>
+				<Card
+					imgUrl="https://fondosmil.com/fondo/3879.jpg"
+					tittle="Card Tittle"
+					description="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+					urlButton="..."
+					labelButton="Find Out More!"
+				/>
+			</div>
+		</div>
+	);
+};
